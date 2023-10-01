3b982ffc606090596be4a821ff759976ce0d0ba2
Mehul Srivastava <mehulsrivastava1206@gmail.com>
2023-10-01 17:56:44 +0800

    chore: react tailwind setup

---
+++ shareme_frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="text-3xl font-bold underline text-red-400">
+      Hello, Mehul!
+    </div>
+  );
+};
+
+export default App;
