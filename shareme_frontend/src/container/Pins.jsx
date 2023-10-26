ef8e7b15f9d3c37986c0c20fdffda6c45b6e16be
Mehul Srivastava <mehulsrivastava1206@gmail.com>
2023-10-26 11:45:51 +0800

    chore: frontend fix

--- shareme_frontend/src/container/Pins.jsx
+++ shareme_frontend/src/container/Pins.jsx
@@ -7,27 +7,25 @@ const Pins = ({ user }) => {
   const [searchTerm, setSearchTerm] = useState("");
 
   return (
-    <div className="px-2 md:px-5">
-      <div className="bg-gray-50">
-        <Navbar
-          searchTerm={searchTerm}
-          setSearchTerm={setSearchTerm}
-          user={user}
-        />
-        <div className="h-full">
-          <Routes>
-            <Route path="/" element={<Feed />} />
-            <Route path="/category/:categoryId" element={<Feed />} />
-            <Route path="/pin/:pinId" element={<PinDetail user={user} />} />
-            <Route path="/create" element={<CreatePin user={user} />} />
-            <Route
-              path="/search"
-              element={
-                <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
-              }
-            />
-          </Routes>
-        </div>
+    <div className="px-2 md:px-5 bg-gray-50">
+      <Navbar
+        searchTerm={searchTerm}
+        setSearchTerm={setSearchTerm}
+        user={user}
+      />
+      <div className="h-full">
+        <Routes>
+          <Route path="/" element={<Feed />} />
+          <Route path="/category/:categoryId" element={<Feed />} />
+          <Route path="/pin/:pinId" element={<PinDetail user={user} />} />
+          <Route path="/create" element={<CreatePin user={user} />} />
+          <Route
+            path="/search"
+            element={
+              <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+            }
+          />
+        </Routes>
       </div>
     </div>
   );
